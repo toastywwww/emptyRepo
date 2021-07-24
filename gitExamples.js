6a8432c1534299b6edfd71c52694d9089e3da7f3
toastywwww <prithvi.paturi@gmail.com>
2021-07-24 12:04:09 -0400

    added more comments

--- gitExamples.js
+++ gitExamples.js
@@ -1,5 +1,7 @@
 // git init - makes a new repo, ONLY DO ONCE FOR EACH PROJECT!
 // git commit - saves mini, changes while coding. Helpful incase you break something and cannot fix it. It's like going back to a fresh start.
 // THIS IS THE LAST COMMENT
-//Git Push origin HEAD - this will push your local code to the remote repo
-//git checkout - this is how you swithc between branches (add -b if its a branch that does not exist yet)
+// git Push origin HEAD - this will push your local code to the remote repo
+// git checkout - this is how you swithc between branches (add -b if its a branch that does not exist yet)
+// git pull origin HEAD - pulls code from remote repo
+// git branch -D branch-name - delete a branch
