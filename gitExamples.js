b3ef6dd5962b5f4a0cbf4e4e4c4c344fb71cd972
toastywwww <prithvi.paturi@gmail.com>
2021-07-24 11:37:22 -0400

    added grammer

--- gitExamples.js
+++ gitExamples.js
@@ -1,3 +1,3 @@
-// git init - makes a new repo, ONLY DO ONCE FOR EACH PROJECT
-// git commit - saves mini changes while coding. Helpful incase you break something and cannot fix it. It's like going back to a fresh start.
+// git init - makes a new repo, ONLY DO ONCE FOR EACH PROJECT!
+// git commit - saves mini, changes while coding. Helpful incase you break something and cannot fix it. It's like going back to a fresh start.
 // THIS IS THE LAST COMMENT
