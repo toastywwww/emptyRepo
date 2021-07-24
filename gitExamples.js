6fad3796c5e4f3ca81e9b206792e81e9e775c8d1
toastywwww <prithvi.paturi@gmail.com>
2021-07-24 11:34:38 -0400

    Added second comment

--- gitExamples.js
+++ gitExamples.js
@@ -1,1 +1,2 @@
 // git init - makes a new repo, ONLY DO ONCE FOR EACH PROJECT
+// git commit - saves mini changes while coding. Helpful incase you break something and cannot fix it. It's like going back to a fresh start.
