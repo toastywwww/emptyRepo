490bcb685bf3fce8540af86d410dd7706cdb7e4e
toastywwww <prithvi.paturi@gmail.com>
2021-07-24 11:58:53 -0400

    Removed un-needed space

--- gitExamples.js
+++ gitExamples.js
@@ -1,6 +1,5 @@
 // git init - makes a new repo, ONLY DO ONCE FOR EACH PROJECT!
 // git commit - saves mini, changes while coding. Helpful incase you break something and cannot fix it. It's like going back to a fresh start.
 // THIS IS THE LAST COMMENT
-
 //Git Push origin HEAD - this will push your local code to the remote repo
 //git checkout - this is how you swithc between branches (add -b if its a branch that does not exist yet)
